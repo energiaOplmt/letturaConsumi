1daba4c2e34a2bc1453d0f78a41728afccedcc10
energiaOplmt <155423517+energiaOplmt@users.noreply.github.com>
2024-01-02 19:07:02 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 window.onload = async function () {
     document.getElementById("sendApi").addEventListener("click", sendApi);
-    // document.getElementById("autoconsumoLink").addEventListener("click", () => {
-    //     chrome.tabs.create({ url: "https://github.com/energiaOplmt/letturaConsumi" });
-    // });
+    document.getElementById("autoconsumoLink").addEventListener("click", () => {
+        chrome.tabs.create({ url: "https://github.com/energiaOplmt/letturaConsumi" });
+     });
     // document.getElementById("linkConsumi").addEventListener("click", () => {
     //     chrome.tabs.create({ url: "https://private.e-distribuzione.it/PortaleClienti/s/curvedicarico" });
     // });
